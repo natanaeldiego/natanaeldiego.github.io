db4e06612b513afb307d160d1cb9d369c9359374
Natanael Diego <natanaeldiego@gmail.com>
2020-07-14 17:41:31 -0300

    first commit

---
+++ precache-manifest.a55715db20c56af9a98856d06359c650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d38fcf10aaa8da99f3940ed6b3ea075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7073cd0dcfd98feebdeb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "be4fb67c5497a04a58e3",
+    "url": "/static/js/2.ae489655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae489655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7073cd0dcfd98feebdeb",
+    "url": "/static/js/main.fab74900.chunk.js"
+  },
+  {
+    "revision": "d6fd2a532d836170201c",
+    "url": "/static/js/runtime-main.9f4f696a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
